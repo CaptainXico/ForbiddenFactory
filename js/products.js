d8faec3f27351549af2a0961a8234b8bdba5dfc4
CaptainXico <147165553+CaptainXico@users.noreply.github.com>
2026-01-05 14:21:57 +0000

    Add Raspberry Pi Control Module to products list

---
+++ js/products.js
@@ -0,0 +1,8 @@
+const products = {
+  piModule01: {
+    name: "Raspberry Pi Control Module",
+    price: "$49",
+    description: "Custom Pi-based control unit with GPIO breakout.",
+    buyLink: "https://example.com",
+  }
+};
