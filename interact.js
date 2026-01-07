86599f1c4929f94f04171f8fb001c81fbe0bd1e2
CaptainXico <147165553+CaptainXico@users.noreply.github.com>
2026-01-07 21:46:12 +0000

    Update interact.js
    VR and PC hover-effect working

--- interact.js
+++ interact.js
@@ -2,15 +2,24 @@ AFRAME.registerComponent('hover-effect', {
   init() {
     const box = this.el.querySelector('a-box');
 
-    this.el.addEventListener('mouseenter', () => {
+    const highlight = () => {
       box.setAttribute('color', '#00ffcc');
-    });
+    };
 
-    this.el.addEventListener('mouseleave', () => {
+    const unhighlight = () => {
       box.setAttribute('color', '#222');
-    });
+    };
+
+    // Desktop (mouse)
+    this.el.addEventListener('mouseenter', highlight);
+    this.el.addEventListener('mouseleave', unhighlight);
+
+    // VR (controllers)
+    this.el.addEventListener('raycaster-intersected', highlight);
+    this.el.addEventListener('raycaster-intersected-cleared', unhighlight);
   }
 });
+
 
 AFRAME.registerComponent('desktop-cursor-only', {
   init() {
