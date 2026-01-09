fdf7dc455b13705301849c7c886bb1a95b61a133
CaptainXico <147165553+CaptainXico@users.noreply.github.com>
2026-01-09 15:25:29 +0000

    Update ui.js
    Revive this file

--- ui.js
+++ ui.js
@@ -1,5 +1,29 @@
-function showProduct(product) {
-  alert(
-    `${product.name}\n\n${product.description}\nPrice: ${product.price}`
-  );
-}
+AFRAME.registerComponent('ui-panel-animate', {
+  schema: {
+    hiddenPosition: { type: 'vec3' },
+    visiblePosition: { type: 'vec3' },
+    duration: { type: 'number', default: 400 }
+  },
+
+  init() {
+    this.el.object3D.position.copy(this.data.hiddenPosition);
+  },
+
+  show() {
+    this.el.setAttribute('animation__in', {
+      property: 'position',
+      to: this.data.visiblePosition,
+      dur: this.data.duration,
+      easing: 'easeOutCubic'
+    });
+  },
+
+  hide() {
+    this.el.setAttribute('animation__out', {
+      property: 'position',
+      to: this.data.hiddenPosition,
+      dur: this.data.duration,
+      easing: 'easeInCubic'
+    });
+  }
+});
